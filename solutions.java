38859a337814783dab11b2a0e9e2e3ac74c57c33
Jenna Kolodny <61959310+jennakolodny99@users.noreply.github.com>
2021-12-03 11:56:46 -0500

    Update solutions.java

--- solutions.java
+++ solutions.java
@@ -1,4 +1,4 @@
-/* Day 1 Part 1*/
+/* Day 1 Part 
 import java.util.Scanner; 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -43,3 +43,53 @@ public class solution{
     }
 
 }
+*/ 
+
+/* Day 1 Part 2
+
+import java.util.Scanner; 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+
+public class solution{
+    public static void main(String[] args){
+
+        
+        try {
+            // open file to read
+            Scanner scanner = new Scanner(new File("input.txt"));
+        
+            // read until end of file (EOF)
+            int counter = 0;
+            int i = 0;
+            int [] depths = new int[2000];
+            while (scanner.hasNextLine()) {
+                int value = scanner.nextInt();
+               // System.out.println(value);
+                depths[i] = value;
+                i++;
+                
+                
+            }
+            
+            for (int j = 2; j < depths.length - 1; j++){
+                //System.out.println(depths[j]);
+                if ((depths[j+1] + depths[j] + depths[j-1]) - (depths[j] + depths[j-1] + depths[j-2]) > 0){
+                    counter++;
+                }
+            }
+            System.out.println(counter);
+            
+        
+            // close the scanner
+            scanner.close();
+                
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
+
+*/
