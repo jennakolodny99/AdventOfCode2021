6f9b74e2f0712f8af2f61162b2c6ee83b1f39dc1
Jenna Kolodny <61959310+jennakolodny99@users.noreply.github.com>
2021-12-05 12:21:09 -0500

    Update solutions.java

--- solutions.java
+++ solutions.java
@@ -144,3 +144,104 @@ public class solution{
 
 }
 */ 
+
+/* Day 3 Part 1
+import java.util.Scanner; 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Vector;
+
+
+public class solution{
+    public static void main(String[] args){
+
+        
+        try {
+            // open file to read
+            Scanner scanner = new Scanner(new File("input4.txt"));
+        
+            //get the length of the binary string
+            String binaryString = scanner.nextLine();
+            int length = binaryString.length();
+            Vector<Integer> v = new Vector<>();
+            v.add(-1); //do this to start the indexing at 1 for math purposes 
+            for (int i = 0; i < length; i++) {
+                char c = binaryString.charAt(i);
+                int value = Character.getNumericValue(c);
+                v.add(value);
+
+            }
+            
+            while (scanner.hasNextLine()) {
+                String s = scanner.nextLine();
+                for (int i = 0; i < length; i++){
+                    char c = s.charAt(i);
+                    int value = Character.getNumericValue(c);
+                    v.add(value);
+                }
+            }
+            
+            int vectorLength = v.size() - 1; //subtract 1 because we have -1 at index 0
+            int numOfStrings = vectorLength / length; //this is the number of binary strings 
+            int [] gamma = new int[length + 1];
+            gamma[0] = -1; //use as placeholder to start indexing at 1
+            for (int i = 1; i <= length; i++){
+                    int zeroCount = 0;
+                    int oneCount = 0;
+                for (int j = i; j < v.size(); j+= length){
+                    int x = v.get(j);
+                    if (x == 0){
+                        zeroCount++;
+                    }
+                    else{
+                        oneCount++;
+                    }
+                }//for
+                if (zeroCount > oneCount){
+                    gamma[i] = 0;
+                }
+                else{
+                    gamma[i] = 1;
+                }
+            }//for
+
+
+            int [] epsilon = new int[length + 1];
+            epsilon[0] = -1;
+            
+            for (int i = 1; i < gamma.length; i++){
+                if (gamma[i] == 0){
+                    epsilon[i] = 1;
+                }
+                else{
+                    epsilon[i] = 0;
+                }
+            }
+
+           
+
+            String gammaString = "";
+            String epsilonString = "";
+            for (int i = 1; i < gamma.length; i++){
+                char g = Character.forDigit(gamma[i], 10);
+                gammaString = gammaString + g;
+                char e = Character.forDigit(epsilon[i], 10);
+                epsilonString = epsilonString + e;
+            }
+
+            int gammaValue = Integer.parseInt(gammaString, 2);
+            int epsilonValue = Integer.parseInt(epsilonString, 2);
+
+            System.out.println(gammaValue * epsilonValue);
+            
+        
+            // close the scanner
+            scanner.close();
+                
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
+*/ 
